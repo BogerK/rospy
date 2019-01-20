a81d030aa209a0e33c6fdbe5e67450707202c08b
Boger <kiril.boger@hs-weingarten.de>
2019-01-20 17:59:54 +0100

    get values for position of a robot and cordinates of the goal points #4

--- kb_171142_prj/src/skynet.py
+++ kb_171142_prj/src/skynet.py
@@ -8,16 +8,20 @@ from goal_publisher.msg import PointArray
 
 class T1000:
     def __init__(self):
-        self.goal = [] #all goals are stored here
         rospy.init_node('skynet') #the node is initialized
+        self.goal = [] #all goals are stored here
         self.vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10) #publishes the velocity commands
         self.pos_sub = rospy.Subscriber('/gazebo/model_states', ModelStates, self.get_state) # reads xyz of a Robot from ModelStates
         self.goals_sub = rospy.Subscriber('/goals', PointArray, self.get_goals) # reads xyz of the goals from PointArray
         self.rate = rospy.Rate(10) #rate for the loop 10Hz
-    def get_state(self,data):
-        
-    def get_goals(self,data):
 
+    def get_state(self,data):
+        self.pose = data.pose[1] #accesses the element #1 of the array of elements in the ModelStates msg (#1 is my Robot)
+        self.pose.position.x
+        self.pose.position.y
+
+    def get_goals(self,PointArray_msg):
+        self.goal = PointArray_msg.goals # accesses the goals from goal_publisher msg
 
 
 #make and run the main function
