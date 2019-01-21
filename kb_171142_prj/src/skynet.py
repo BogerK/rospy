1410f552a58d41db356ac08a9c24f55d107253ab
Boger <kiril.boger@hs-weingarten.de>
2019-01-21 01:08:34 +0100

    previous strategy confused me and I started from the scretch. Now I have basic structure of a class and all the impotant values stored in arrays for easy acces #4

--- kb_171142_prj/src/skynet.py
+++ kb_171142_prj/src/skynet.py
@@ -7,34 +7,44 @@ from gazebo_msgs.msg import ModelStates
 from tf.transformations import euler_from_quaternion
 from geometry_msgs.msg import Twist
 from goal_publisher.msg import PointArray
+from math import pow, sqrt, degrees
 
 class T1000:
+    cur_position = [3]
+    w = 3
+    h = 2
+    goal = [[0 for x in range(w)] for y in range(h)]  #all goals are stored here
     def __init__(self):
         rospy.init_node('skynet') #the node is initialized
-        self.goal = [] #all goals are stored here
         self.vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10) #publishes the velocity commands
         self.pos_sub = rospy.Subscriber('/gazebo/model_states', ModelStates, self.get_state) # reads xyz of a Robot from ModelStates
         self.goals_sub = rospy.Subscriber('/goals', PointArray, self.get_goals) # reads xyz of the goals from PointArray
-        self.rate = rospy.Rate(10) #rate for the loop 10Hz
+        self.rate = rospy.Rate(10) #frequency of the loop 10Hz
 
-    def get_state(self,data):
-        self.pose = data.pose[1] #accesses the element #1 of the array of elements in the ModelStates msg (#1 is my Robot)
-        self.pose.position.x
-        self.pose.position.y
+    def get_state(self, data): # get the coordinates and deflection of a robot and stores them in to the array
+        rate = rospy.Rate(4) #frequency of the loop 4Hz
+        quaternion = (round((data.pose[1].orientation.x),3), round((data.pose[1].orientation.y),3), round((data.pose[1].orientation.z),3), round((data.pose[1].orientation.w),3))
+        self.cur_position = (round((data.pose[1].position.x), 3), round(( data.pose[1].position.y), 3), round((degrees(euler_from_quaternion(quaternion)[2])), 3))
 
-    def get_goals(self,PointArray_msg):
-        self.goal = PointArray_msg.goals # accesses the goals from goal_publisher msg
+        #print ('position X is: ' +str(self.cur_position[0]))
+        #print ('position Y is: ' +str(self.cur_position[1]))
+        #print ('angle    Z is: ' +str(self.cur_position[2]) +'\n')
+        rate.sleep()
 
-    def e_dist(self): #euclidean distance between pos of a robot and goal point
+    def get_goals(self, data):
+        rate = rospy.Rate(4) #frequency of the loop 4Hz
+        (self.goal[0][0], self.goal[1][0]) = (data.goals[0].x, data.goals[0].y)
+        (self.goal[0][1], self.goal[1][1]) = (data.goals[1].x, data.goals[1].y)
+        (self.goal[0][2], self.goal[1][2]) = (data.goals[2].x, data.goals[2].y)
 
-            return dist
+
+        #print('goal 1 = ' + str(self.goal[0][0]) + ', ' + str(self.goal[1][0]))
+        #print('goal 2 = ' + str(self.goal[0][1]) + ', ' + str(self.goal[1][1]))
+        #print('goal 3 = ' + str(self.goal[0][2]) + ', ' + str(self.goal[1][2]))
+        rate.sleep()
 
     def reach_goals(self):
-        var2 = Twist()
-        while True:
-            var3.linear.x = 0.3
-            var3.angular.z = 0.0
-            self.vel_pub.publish(var2)
+        rospy.spin()
 
 #make and run the main function
 if __name__ == '__main__':
