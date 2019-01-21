f3db1b9a7db332f51d1cc1ca70481c6a79ee2963
Boger <kiril.boger@hs-weingarten.de>
2019-01-22 00:33:48 +0100

    definded m-line and robot is looking for it #4

--- kb_171142_prj/src/skynet.py
+++ kb_171142_prj/src/skynet.py
@@ -8,7 +8,7 @@ from gazebo_msgs.msg import ModelStates
 from tf.transformations import euler_from_quaternion
 from geometry_msgs.msg import Twist
 from goal_publisher.msg import PointArray
-from math import pow, sqrt, degrees, atan2
+from math import pow, sqrt, degrees, atan2, pi
 
 class T1000:
 
@@ -28,7 +28,6 @@ class T1000:
         #print ('angle    Z is: ' +str(self.cur_position[2]) +'\n')
 
     def get_goals(self, PointArray): #get the coordinates of the goals and store them in the array
-
         run_once = 0
         while 1:
             if run_once == 0:
@@ -39,30 +38,56 @@ class T1000:
                 #print('goal 3 = ' + str(self.goals[2].x) + ', ' + str(self.goals[2].y) +'\n')
                 run_once = 1
 
-
-
     def e_dist(self, g_pos_x, g_pos_y): #calculates the euclidian distace between the robot and the goal point
         return sqrt(pow((g_pos_x - self.cur_position[0]), 2) + pow((g_pos_y - self.cur_position[1]), 2))
 
-    def deflection(self, g_pos_x, g_pos_y): #deflection of a robot with respect to goal point
+    def robot_angle(self, g_pos_x, g_pos_y): #angle between X-axis, robot,  goal
         return round(atan2(g_pos_y - self.cur_position[1], g_pos_x - self.cur_position[0]), 3)
 
-    def m_line1_angle(self): #angle between the point of origin and first goal
+    def m_line1_angle(self, dest_x, dest_y, orig_x, orig_y): #angle between X-axis, point of origin, goal
         return round(atan2(self.goals[0].y - 0, self.goals[0].x - 0), 3)
 
+    #def deflection(self, )
 
     def reach_goals(self):
         time.sleep(1)
         speed = Twist()
         while not rospy.is_shutdown():
-            if self.goals:
-                rate=rospy.Rate(1)
-                self.e_dist(self.goals[0].x, self.goals[0].y)
-                #print('the distance is ' +str(self.e_dist(self.goals[0].x, self.goals[0].y)))
-                #self.deflection(self.goals[0].x, self.goals[0].y)
-                print('angle (robot to goal1) is ' +str(self.deflection(self.goals[0].x, self.goals[0].y)))
-                print('angle (origin to goal1) is ' +str(self.m_line1_angle()))
-                rate.sleep()
+            #rate=rospy.Rate(10)
+            self.e_dist(self.goals[0].x, self.goals[0].y)
+            self.robot_angle(self.goals[0].x, self.goals[0].y)
+            self.m_line1_angle(self.goals[0].x, self.goals[0].y, 0,0)
+            var = self.m_line1_angle(self.goals[0].x, self.goals[0].y, 0,0) - self.robot_angle(self.goals[0].x, self.goals[0].y)
+            print('difference is: ' +str(var))
+            print('current z is: ' +str(self.cur_position[2]))
+
+            if var <-0.01 and 0.02<=self.cur_position[2]<=3.12:
+                speed.linear.x = 0
+                speed.angular.z = 0.4
+                #self.vel_pub.publish(speed)
+            elif var <0.01 and -3.12<=self.cur_position[2]<=-0.02:
+                speed.linear.x = 0
+                speed.angular.z = -0.4
+                #self.vel_pub.publish(speed)
+            elif var >0.01 and 0.02<=self.cur_position[2]<=3.12:
+                speed.linear.x = 0
+                speed.angular.z = -0.4
+                #self.vel_pub.publish(speed)
+            elif var >0.01 and -3.12<=self.cur_position[2]<=-0.02:
+                speed.linear.x = 0
+                speed.angular.z = 0.4
+                #self.vel_pub.publish(speed)
+            else:
+                speed.linear.x = 0
+                speed.angular.z = 0.0
+            self.vel_pub.publish(speed)
+
+            #print('the distance is ' +str(self.e_dist(self.goals[0].x, self.goals[0].y)))
+            #print('angle (robot to goal1) is ' +str(self.robot_angle(self.goals[0].x, self.goals[0].y)))
+            #print('angle (origin to goal1) is ' +str(self.m_line1_angle(self.goals[0].x, self.goals[0].y, 0,0)))
+
+            #rate.sleep()
+
         self.rate.sleep()
 
 #make and run the main function
